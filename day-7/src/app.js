396c36b23cf9cc721f9e15a9c12b9b38700e8e71
Baria jayesh <bariajayesh266@gmail.com>
2026-03-16 23:40:46 +0530

    Update day-7 notes API

--- day-7/src/app.js
+++ day-7/src/app.js
@@ -35,22 +35,56 @@ app.post("/notes", async (req, res) => {
  * -fatch all note data
  */
 
-app.get("/notes", async(req,res)=>{
+app.get("/notes", async (req, res) => {
 
-   const note = await noteModel.find()
+    const note = await noteModel.find()
 
-   res.status(200).json({
-        
-        massage:"Note successfully fatch..",
+    res.status(200).json({
+        massage: "Note successfully fatch..",
         note
-    
-   })
-    
+    })
+
 })
 
+/**
+ * delete notes/:id
+ * Delete note with the id from req.params
+ */
+
+app.delete('/notes/:id', async (req, res) => {
+    const id = req.params.id
+
+    const deletenote = await noteModel.findByIdAndDelete(id)
+
+    if (!deletenote) {
+        return res.status(400).json({
+            massage: "Note Not Found"
+        })
+    }
+
+    res.status(200).json({
+        message: "Note successfully deleted.",
+        deletenote
+    })
+})
+
+/**
+ * Patch notes/:id
+ * update the description of the note by id
+ * req.body = {description}
+ */
+
+app.patch("/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const { description } = req.body
+
+    const updatedNote = await noteModel.findByIdAndUpdate (id, { description },{new:true})
 
 
-
-
+    res.status(200).json({
+        message: "Note Successfully Updated.",
+        updatedNote
+    })
+})
 
 module.exports = app
